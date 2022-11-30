0d186709706bb864c97ef8ab047b08c082a8a3d9
Juliettedqb <juliettedqb@gmail.com>
2022-11-30 15:03:13 +0100

    update script

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 function buildUrl(name, year) {
-    return `https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_prenoms-enfants-nes-nantes&q=&rows=100&sort=annee_naissance&facet=prenom&facet=sexe&facet=annee_naissance&refine.annee_naissance=${year}&refine.prenom=${name.toUpperCase()}`;
-  }
-  
-  function getOccurrence(name, year) {
-    fetch(buildUrl(name, year)).then((x) => {
-      x.json().then((y) => {
-        let sum = 0;
-        for (let i = 0; i < y.records.length; i++) {
-          sum += y.records[i].fields.occurrence;
-        }
-        console.log(sum);
-      });
-    });
-  }
-  
-  getOccurrence("Juliette","2020")
-  
+  return `https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_prenoms-enfants-nes-nantes&q=&rows=100&sort=annee_naissance&facet=prenom&facet=sexe&facet=annee_naissance&refine.annee_naissance=${year}&refine.prenom=${name.toUpperCase()}`;
+}
+
+// function getOccurrence(name, year) {
+//   fetch(buildUrl(name, year)).then((x) => {
+//     console.log(x)
+//     x.json().then((y) => {
+//       let sum = 0;
+//       for (let i = 0; i < y.records.length; i++) {
+//         sum += y.records[i].fields.occurrence;
+//       }
+//       console.log(sum);
+//     });
+//   });
+// }
+
   async function getOccurrence(name, year) {
     const result = await fetch(buildUrl(name, year));
     const result2 = await result.json();
@@ -25,8 +24,41 @@ function buildUrl(name, year) {
     }
     console.log(sum);
   }
-  
-  getOccurrence("Jean", "2011");
-  getOccurrence("Juliette", "2010");
-  getOccurrence("Camille", "2012")
-  getOccurrence("Théo", "2002")
+
+
+function buildUrl2(year) {
+  return `https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_prenoms-enfants-nes-nantes&q=&rows=1000&sort=annee_naissance&facet=prenom&facet=sexe&facet=annee_naissance&refine.annee_naissance=${year}`;
+}
+
+// function getBirthPerYear(year) {
+//   fetch(buildUrl2(year)).then((x) => {
+//     console.log(x);
+//     x.json().then((y) => {
+//       console.log(y);
+//       let sum = 0
+//       for(let i=0; i< y.records.length; i++) {
+//         sum += y.records[i].fields.occurrence;
+//       }
+//       console.log(sum)
+//     })
+//   });
+// }
+
+async function getBirthPerYear(year, gender) {
+  const result = await fetch(buildUrl2(year));
+  const result2 = await result.json();
+  console.log(result2);
+  let sum = 0;
+  for(let i=0; i< result2.records.length; i++) {
+    if(result2.records[i].fields.sexe == gender){
+      sum += result2.records[i].fields.occurrence;
+    }
+  }
+  console.log(sum);
+};
+
+getBirthPerYear(2021,"FILLE");
+getBirthPerYear(2021, "GARCON");
+getOccurrence("Juliette", 2021);
+
+
