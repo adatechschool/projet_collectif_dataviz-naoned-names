b8ff49d3415fbf7ac01e4059834a998710cb8f27
Juliettedqb <juliettedqb@gmail.com>
2022-12-06 06:13:18 +0100

    add new script

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function buildUrl(name, year) {
-  return `https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_prenoms-enfants-nes-nantes&q=&rows=100&sort=annee_naissance&facet=prenom&facet=sexe&facet=annee_naissance&refine.annee_naissance=${year}&refine.prenom=${name.toUpperCase()}`;
-}
-
-async function getOccurrence(name, year) {
-  const result = await fetch(buildUrl(name, year));
-  const result2 = await result.json();
-  let sum = 0;
-  for (let i = 0; i < result2.records.length; i++) {
-    sum += result2.records[i].fields.occurrence;
-  }
-  console.log(sum);
-}
-
 function buildUrl2(year) {
   return `https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_prenoms-enfants-nes-nantes&q=&rows=1000&sort=annee_naissance&facet=prenom&facet=sexe&facet=annee_naissance&refine.annee_naissance=${year}`;
 }
@@ -25,34 +11,58 @@ async function getBirthPerYear(year, gender) {
       sum += result2.records[i].fields.occurrence;
     }
   }
-  return sum
+  return sum;
 }
 
 (async () => {
   // const first = await getBirthPerYear(2010, "FILLE");
   // const second = await getBirthPerYear(2011, "FILLE");
 
-  let arrBirthRateF = []
-  let arrYears = [2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2020,2021];
+  let arrBirthRateF = [];
+  let arrYears = [
+    2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012,
+    2013, 2014, 2015, 2016, 2017, 2018, 2020, 2021,
+  ];
 
-  for(let i=0; i<arrYears.length; i++){
-    await arrBirthRateF.push(await getBirthPerYear(arrYears[i],"FILLE"))
+  for (let i = 0; i < arrYears.length; i++) {
+    await arrBirthRateF.push(await getBirthPerYear(arrYears[i], "FILLE"));
   }
-  console.log(arrBirthRateF)
+  console.log(arrBirthRateF);
 
-  let arrBirthRateM = []
+  let arrBirthRateM = [];
 
-  for(let i=0; i<arrYears.length; i++){
-    await arrBirthRateM.push(await getBirthPerYear(arrYears[i],"GARCON"))
+  for (let i = 0; i < arrYears.length; i++) {
+    await arrBirthRateM.push(await getBirthPerYear(arrYears[i], "GARCON"));
   }
-  console.log(arrBirthRateM)
+  console.log(arrBirthRateM);
 
   const ctx = document.getElementById("myChart");
 
   new Chart(ctx, {
     type: "line",
     data: {
-      labels: ["2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2020","2021"],
+      labels: [
+        "2001",
+        "2002",
+        "2003",
+        "2004",
+        "2005",
+        "2006",
+        "2007",
+        "2008",
+        "2009",
+        "2010",
+        "2011",
+        "2012",
+        "2013",
+        "2014",
+        "2015",
+        "2016",
+        "2017",
+        "2018",
+        "2020",
+        "2021",
+      ],
       datasets: [
         {
           label: "female birth rate",
---
+++ script2.js
@@ -0,0 +1,75 @@
+function buildUrl(name, year) {
+  return `https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_prenoms-enfants-nes-nantes&q=&rows=100&sort=annee_naissance&facet=prenom&facet=sexe&facet=annee_naissance&refine.annee_naissance=${year}&refine.prenom=${name.toUpperCase()}`;
+}
+
+async function getOccurrence(name, year) {
+  const result = await fetch(buildUrl(name, year));
+  const result2 = await result.json();
+  let sum = 0;
+  for (let i = 0; i < result2.records.length; i++) {
+    sum += result2.records[i].fields.occurrence;
+  }
+  return sum;
+}
+
+async function getValue() {
+  // Sélectionner l'élément input et récupérer sa valeur
+  let input = document.getElementById("in").value;
+  // Afficher la valeur
+  // alert(input);
+
+  let arrOccurrence = [];
+  let arrYears = [
+    2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012,
+    2013, 2014, 2015, 2016, 2017, 2018, 2020, 2021,
+  ];
+
+  for (let i = 0; i < arrYears.length; i++) {
+    arrOccurrence.push(await getOccurrence(input, arrYears[i]));
+  }
+  console.log(arrOccurrence);
+
+  const ctx = document.getElementById("myChart");
+
+  new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: [
+        "2001",
+        "2002",
+        "2003",
+        "2004",
+        "2005",
+        "2006",
+        "2007",
+        "2008",
+        "2009",
+        "2010",
+        "2011",
+        "2012",
+        "2013",
+        "2014",
+        "2015",
+        "2016",
+        "2017",
+        "2018",
+        "2020",
+        "2021",
+      ],
+      datasets: [
+        {
+          label: "Occurrence du prénom " + `${input}`,
+          data: arrOccurrence,
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+}
